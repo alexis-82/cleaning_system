92a72ac2dac5ad4e65dd1b349da3f4821da56ca0
alexis-82 <giobbia82@gmail.com>
2024-07-02 21:46:02 +0200

    - add feature cache

--- clean.py
+++ clean.py
@@ -15,6 +15,7 @@ if root == "sudo":
     time.sleep(1)
     print()
     print("Inizio sistema di aggiornamento\n")
+    print()
     os.system("sudo apt-get update")
     os.system("sudo apt-get upgrade -y")
 
@@ -22,28 +23,40 @@ if root == "sudo":
     print()
     print("Installatzione di Deborphan\n")
     time.sleep(1)
+    print()
     os.system("sudo apt-get install deborphan")
 
     # Apply Deborphan
     time.sleep(1)
     print()
     print("Rimozione librerie non utilizzate\n")
+    print()
     os.system("sudo deborphan | xargs sudo apt-get -y remove --purge")
     
     time.sleep(1)
-    print() 
     print("Inizio sistema di pulizia\n")
     print("Clean...")
+    print()
     os.system("sudo apt-get clean")
     print("Autoclean...")
+    print()
     os.system("sudo apt-get autoclean")
+    print()
     print("Autoremove...")
     os.system("sudo apt-get autoremove")
 
     time.sleep(1)
     print()
     print("Riduzione uso RAM\n")
+    print()
     os.system("sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a'")
+    
+    time.sleep(1)
+    print()
+    print("Pulizia Cache\n")
+    print()
+    os.system("rm -rf ~/.cache/*")
+
 
 else:
     time.sleep(2)
@@ -52,12 +65,14 @@ else:
     time.sleep(1)
     print()
     print("Inizio sistema di aggiornamento\n")
+    print()
     os.system("su root -c 'apt-get update'")
     os.system("su root -c 'apt-get upgrade -y'")
     
     # Installation library Deborphan
     print()
     print("Installatzione di Deborphan\n")
+    print()
     time.sleep(1)
     os.system("su root -c 'apt-get install deborphan'")
     
@@ -65,11 +80,13 @@ else:
     time.sleep(1)
     print()
     print("Rimozione librerie non utilizzate\n")
+    print()
     os.system("su root -c 'deborphan | xargs apt-get -y remove --purge'") 
     
     time.sleep(1)
     print()
-    print("Inizio sistema di pulizia\n")                                                                                                                                                   
+    print("Inizio sistema di pulizia\n") 
+    print()                                                                                                                                                  
     print("Clean...")
     os.system("su root -c 'apt-get clean'")
     print("Autoclean...")
@@ -80,5 +97,12 @@ else:
     time.sleep(1)
     print()
     print("Riduzione uso RAM\n")
+    print()
     os.system("su root -c 'echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a'")
+    
+    time.sleep(1)
+    print()
+    print("Pulizia Cache\n")
+    print()
+    os.system("su root -c 'rm -rf ~/.cache/*'")
 
